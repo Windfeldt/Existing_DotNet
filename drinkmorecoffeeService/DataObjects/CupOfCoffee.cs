161cce0b33fb81a7c311353bf99076fd8e3a8b20
Rasmus <pippihippi.hippi@gmail.com>
2019-04-03 14:16:35 +0200

    Add project files.

---
+++ drinkmorecoffeeService/DataObjects/CupOfCoffee.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace drinkmorecoffeeService.DataObjects
+{
+    public class CupOfCoffee : EntityData
+    {
+        public DateTime DateUtc { get; set; }
+        public bool MadeAtHome { get; set; }
+        public string os { get; set; }
+    }
+}
